5c60b249f578c1f2e7dcf79ce3efc451b406aec7
Nathan Daly <nathandalypretoria@hotmail.co.uk>
2021-06-07 22:08:56 +0100

    Adding migration to create the specified `transactions` schema.

---
+++ database/migrations/2021_06_07_151803_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')
+                ->comment('Transaction title for identification');
+
+            $table->decimal('amount', 12)
+                ->comment('Transaction amount up to two decimal places');
+
+            $table->foreignId('author_id')
+                ->comment('References the User ID.')
+                ->constrained('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
