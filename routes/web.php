55df7c976c132919342f913a3364a28b5830795f
Nathan Daly <nathandalypretoria@hotmail.co.uk>
2021-06-08 00:13:42 +0100

    Moving api routing decleration to correct routing file.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +16,3 @@ use App\Http\Controllers\TransactionController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::apiResource('transactions', TransactionController::class)
-    ->except([
-        'show',
-        'update',
-    ]);
